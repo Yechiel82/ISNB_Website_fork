bed56c561d1b174cb96357bdfb3dd02130d4aec8
yechiel <yechielarianto@gmail.com>
2024-04-27 14:26:12 +0700

    Your commit message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export'
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
-}
+  images: {
+    unoptimized: true
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
