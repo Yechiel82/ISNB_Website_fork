72eeda2b8ff6514ceb7a5997f30d496e10926aaa
yechiel <yechielarianto@gmail.com>
2024-05-01 21:07:34 +0700

    please work

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+// pages/404.js or pages/404.tsx
+
+import React from 'react';
+
+function Custom404() {
+  return <div>Custom 404 Page</div>;
+}
+
+export default Custom404;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+// pages/index.js or pages/index.tsx
+
+import React from 'react';
+
+const HomePage = () => {
+  return <div>Welcome to the home page!</div>;
+};
+
+export default HomePage;
